3aa1c5a2c432bd6841ee05d1a2d9a6969d48ba24
kh-d-lab <krazex@naver.com>
2022-04-21 21:48:03 +0900

    테스트 커밋

---
+++ SungJuk_Ver2.java
@@ -0,0 +1,20 @@
+package git_test;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class SungJuk_Ver2 {
+	JFrame     jtf     = new JFrame();
+	JLabel     jlb_kor = new JLabel("국어");
+	JTextField jft_kor = new JTextField(10);
+	JLabel     jlb_eng = new JLabel("영어");
+	JTextField jft_eng = new JTextField(10);
+	public static void initDisplay() {
+		
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
